c51feb0a0fb01bb73915a2ff5d69933f6c709763
Jeff Marks <jeff.t.marks@gmail.com>
2022-10-09 12:22:04 -0400

    done with assignment

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,5 +1,14 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+	const arr = string.split("");
+	
+	for (let i = 0; i < arr.length; i++) {
+		(arr.slice(i)).reduce((prev, curr) => {
+			result.push(prev + curr);
+			return prev + curr
+		}, "");
+	}
+	return result;
 }
 
 if (require.main === module) {
@@ -17,3 +26,8 @@ module.exports = consecutiveSubstrings;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/**
+ * Iterate through string, letter by letter
+ * For each letter, perform a second iteration through array and push each substring to a result array
+ */
